91cba6f434a82d875a2884b6fa67bce0b6a7cd7b
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-01-20 18:13:44 -0300

    C#: Added class38 ex1047

---
+++ C#_primeiros_passos--Logica_de_Programacao_e_Algoritmos/projetos/class38_exerciciosPropostos/ex1047/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApplication2 {
+  class Program {
+    static void Main(string[] args) {
+      
+        Console.Clear();
+        Console.WriteLine("Exercício 1047");
+        Console.WriteLine("Tempo de Jogo com Minutos");
+        Console.WriteLine("4 Valores/int");
+        Console.WriteLine("------------------------");
+
+        int horaInicial, minutoInicial, horaFinal, minutoFinal, tempoEmMinutoInicial, tempoEmMinutoFinal, duracaoEmMinutos = 0;
+        int horasDeDuracao, minutosDeDuracao;
+        string[] v = Console.ReadLine().Split(' ');
+        horaInicial = int.Parse(v[0]);
+        minutoInicial = int.Parse(v[1]);
+        horaFinal = int.Parse(v[2]);
+        minutoFinal = int.Parse(v[3]);
+
+        tempoEmMinutoInicial = ((horaInicial * 60) + minutoInicial);
+        tempoEmMinutoFinal = ((horaFinal * 60) + minutoFinal);
+
+        if(tempoEmMinutoInicial < tempoEmMinutoFinal){
+            duracaoEmMinutos = tempoEmMinutoFinal - tempoEmMinutoInicial;
+        } else {
+            duracaoEmMinutos = ((24 * 60) -tempoEmMinutoInicial) + tempoEmMinutoFinal;
+        }
+
+        horasDeDuracao = duracaoEmMinutos / 60;
+        minutosDeDuracao = duracaoEmMinutos % 60;        
+
+        Console.WriteLine("O JOGO DUROU "+ horasDeDuracao +" HORA(S) E "+ minutosDeDuracao+ " MINUTO(S)");
+        
+        Console.WriteLine("------------------------");
+        Console.ReadLine();
+    }
+  }
+};
