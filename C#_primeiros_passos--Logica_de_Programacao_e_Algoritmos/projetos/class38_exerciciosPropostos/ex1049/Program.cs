d024243e660b779c15634b0322b9281eba711c1e
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-01-22 11:14:11 -0300

    C#: Added class38 ex1049

---
+++ C#_primeiros_passos--Logica_de_Programacao_e_Algoritmos/projetos/class38_exerciciosPropostos/ex1049/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApplication2 {
+  class Program {
+    static void Main(string[] args) {
+      
+        Console.Clear();
+        Console.WriteLine("Exercício 1049");
+        Console.WriteLine("Animal");
+        Console.WriteLine("3 palavras/string");
+        Console.WriteLine("------------------------");
+
+        string classificacao, tipo, alimentacao, animal = "";
+
+        classificacao = Console.ReadLine();
+        tipo = Console.ReadLine();
+        alimentacao = Console.ReadLine();
+
+        if(classificacao == "vertebrado"){
+            if(tipo == "ave"){
+                if(alimentacao == "carnivoro"){
+                    animal = "aguia";
+                } else if (alimentacao == "onivoro"){
+                    animal = "pomba";
+                }
+            } else if(tipo == "mamifero"){
+                if(alimentacao == "onivoro"){
+                    animal = "homem";
+                } else if(alimentacao == "herbivoro"){
+                    animal = "vaca";
+                }
+            }
+        } else if(classificacao == "invertebrado"){
+            if(tipo == "inseto"){
+                if(alimentacao == "hematofago"){
+                    animal = "pulga";
+                } else if(alimentacao == "herbivoro"){
+                    animal = "lagarta";
+                }
+            } else if(tipo == "anelideo"){
+                if(alimentacao == "hematofago"){
+                    animal = "sanguessuga";
+                } else if(alimentacao == "onivoro"){
+                    animal = "minhoca";
+                }
+            }
+        }
+
+        Console.WriteLine(animal);
+        
+        Console.WriteLine("------------------------");
+        Console.ReadLine();
+    }
+  }
+};
