01f1656a091acad9e0306cd0dccbf5ff0a9878ab
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-02-16 16:56:42 -0300

    C#: Added class67 and class68 from ex01 to ex03

---
+++ C#_primeiros_passos--Logica_de_Programacao_e_Algoritmos/projetos/class68_exerciciosPropostos/ex03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApplication2 {
+  class Program {
+    static void Main(string[] args) {
+      
+        Console.Clear();
+        Console.WriteLine("Aula 68 - Exercícios Propostos");
+        Console.WriteLine("ex03");
+        Console.WriteLine("------------------------");
+
+        // declaração de variáveis e entrada de dados
+        int ordemDaMatriz = int.Parse(Console.ReadLine());
+        int[,] matriz = new int[ordemDaMatriz, ordemDaMatriz];
+        
+        for (int linhas = 0; linhas < ordemDaMatriz; linhas++) {
+            string[] valor = Console.ReadLine().Split(" ");
+            for (int colunas = 0; colunas < ordemDaMatriz; colunas++) {
+                matriz[linhas, colunas] = int.Parse(valor[colunas]);
+            };
+        };
+
+        //processamento e saída de dados
+        for (int linhas = 0; linhas < ordemDaMatriz; linhas++) {
+            int maiorElemento = 0;
+            for (int colunas = 0; colunas < ordemDaMatriz; colunas++) {
+                if ( matriz[linhas, colunas] > maiorElemento) {
+                    maiorElemento = matriz[linhas, colunas];
+                };
+            };
+            Console.WriteLine(maiorElemento);
+        };
+
+        Console.WriteLine("------------------------");
+        Console.ReadLine();
+    }
+  }
+};
